b6008600a675da44e1fcde5b7c8c79b16b1fdb57
Danni Popova <dannii.popova@gmail.com>
2022-12-09 13:16:31 +0000

    Redirect to todanni.com/tasks

--- service/auth/service.go
+++ service/auth/service.go
@@ -144,7 +144,7 @@ func (s *authService) CallbackHandler(w http.ResponseWriter, r *http.Request) {
 	})
 
 	w.Header().Set("Content-Type", "application/json")
-	http.Redirect(w, r, "/tasks", http.StatusFound)
+	http.Redirect(w, r, "https://todanni.com/tasks", http.StatusFound)
 }
 
 type GoogleUserInfo struct {
