7a67107356a20f76be570f5e8bc723e3153bd572
Danni Popova <dannii.popova@gmail.com>
2022-12-09 13:19:51 +0000

    Redirect to the URL from the oauth config

--- service/auth/service.go
+++ service/auth/service.go
@@ -144,7 +144,7 @@ func (s *authService) CallbackHandler(w http.ResponseWriter, r *http.Request) {
 	})
 
 	w.Header().Set("Content-Type", "application/json")
-	http.Redirect(w, r, "https://todanni.com/tasks", http.StatusFound)
+	http.Redirect(w, r, s.oauthConfig.RedirectURL, http.StatusFound)
 }
 
 type GoogleUserInfo struct {
